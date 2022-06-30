34da9c0a0b76fb51645307d1c965d7df032381bb
Lucio Ramos <lucio.a.ramos@gmail.com>
2022-06-30 16:36:52 -0300

    Se realiza la entrega de Simulador interactivo.

--- main.js
+++ main.js
@@ -1,36 +1,101 @@
-/* Ejercicio con for */
-
-for (let i = 1; i <= 10; i++) {
-  let table = 2;
-  let resultado = i * table;
-  console.log(i + "x" + table + " = " + resultado);
+let primerParcial;
+let segundoParcial;
+let asistencia;
+let condicion;
+let loop = true;
+alert(
+  "Para aprobar este curso es necesario tener nota mayor a 7 en los dos parciales, ademas tener el 80% asistencia de las 30 clases dictadas."
+);
+function condicionAlumno(primerParcial, segundoParcial, asistencia) {
+  while (loop) {
+    if (
+      primerParcial >= 4 &&
+      primerParcial <= 10 &&
+      segundoParcial >= 4 &&
+      segundoParcial <= 10 &&
+      asistencia >= 30 * 0.8 &&
+      asistencia <= 30
+    ) {
+      alert("Felicitaciones! Has aprobado el curso!");
+      loop = false;
+      condicion = "Aprobado";
+    } else if (
+      primerParcial < 4 &&
+      primerParcial <= 10 &&
+      segundoParcial >= 4 &&
+      segundoParcial <= 10 &&
+      asistencia >= 30 * 0.8 &&
+      asistencia <= 30
+    ) {
+      alert("Para poder aprobar el curso deberas recuperar el Primer Parcial.");
+      loop = false;
+      condicion = "Condicional";
+    } else if (
+      primerParcial < 4 &&
+      primerParcial <= 10 &&
+      segundoParcial >= 4 &&
+      segundoParcial <= 10 &&
+      asistencia < 30 * 0.8 &&
+      asistencia <= 30
+    ) {
+      alert(
+        "No has asistido al 80% de las clases ni tampoco aprobado el Primer Parcial, deberás volver a tomar el curso."
+      );
+      loop = false;
+      condicion = "Reprobado";
+    } else if (
+      primerParcial >= 4 &&
+      primerParcial <= 10 &&
+      segundoParcial < 4 &&
+      segundoParcial <= 10 &&
+      asistencia >= 30 * 0.8 &&
+      asistencia <= 30
+    ) {
+      alert(
+        "Para poder aprobar el curso deberas recuperar el Segundo Parcial."
+      );
+      loop = false;
+      condicion = "Condicional";
+    } else if (
+      primerParcial >= 4 &&
+      primerParcial <= 10 &&
+      segundoParcial < 4 &&
+      segundoParcial <= 10 &&
+      asistencia < 30 * 0.8 &&
+      asistencia <= 30
+    ) {
+      alert(
+        "No has asistido al 80% de las clases ni tampoco aprobado el Segundo Parcial, deberás volver a tomar el curso."
+      );
+      loop = false;
+      condicion = "Reprobado";
+    } else if (
+      primerParcial >= 4 &&
+      primerParcial <= 10 &&
+      segundoParcial >= 4 &&
+      segundoParcial <= 10 &&
+      asistencia < 30 * 0.8 &&
+      asistencia <= 30
+    ) {
+      alert(
+        "Has cumplimentado con la nota de los parciales pero no has asistido al 80% de las clases. Por lo tanto, deberás volver a recursar."
+      );
+      loop = false;
+      condicion = "Reprobado";
+    } else {
+      alert("Ingresa un valor válido por favor.");
+      loop = false;
+      condicion =
+        "Hasta que no ingreses valores válidos no vamos a poder indicarte tu condición.";
+    }
+  }
+  return condicion;
 }
 
-/* Ejercicio con while */
+let condicionFinal = condicionAlumno(
+  parseInt(prompt("Ingresa la nota que te sacaste en el Primer Parcial: ")),
+  parseInt(prompt("Ingresa la nota que te sacaste en el Segundo Parcial: ")),
+  parseInt(prompt("Ingresa la cantidad de clases a la que has asistido: "))
+);
 
-let nivel = 2;
-let exp = 100;
-
-while (nivel <= 5) {
-  console.log("Te has encontrado un monstruo!");
-  console.log("Decides atacarlo");
-  let expUp = exp;
-  exp *= 2;
-  console.log("Lo venciste! Ganaste " + expUp + "xp");
-  let levelUp = nivel++;
-  console.log("Subiste a nivel " + levelUp);
-}
-
-/* Ejercicio con do while */
-
-let level = 6;
-let xp = 1500;
-do {
-  console.log("Te has encontrado un monstruo!");
-  console.log("Decides atacarlo");
-  let expUp = xp;
-  xp *= 2;
-  console.log("Lo venciste! Ganaste " + expUp + "xp");
-  let levelUp = level++;
-  console.log("Subiste a nivel " + levelUp);
-} while (nivel <= 5);
+alert("Tu condición como alumnx es: " + condicionFinal);
