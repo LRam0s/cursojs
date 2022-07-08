f0bea38bb6659dc89ec01cf62801e3827d37bb98
Lucio Ramos <lucio.a.ramos@gmail.com>
2022-07-08 11:29:18 -0300

    Se utilizan objetos y array, se prueba con otro tipo de contenido sobre el cual trabajar

--- main.js
+++ main.js
@@ -1,101 +1,116 @@
-let primerParcial;
-let segundoParcial;
-let asistencia;
-let condicion;
-let loop = true;
-alert(
-  "Para aprobar este curso es necesario tener nota mayor a 7 en los dos parciales, ademas tener el 80% asistencia de las 30 clases dictadas."
-);
-function condicionAlumno(primerParcial, segundoParcial, asistencia) {
-  while (loop) {
-    if (
-      primerParcial >= 4 &&
-      primerParcial <= 10 &&
-      segundoParcial >= 4 &&
-      segundoParcial <= 10 &&
-      asistencia >= 30 * 0.8 &&
-      asistencia <= 30
-    ) {
-      alert("Felicitaciones! Has aprobado el curso!");
-      loop = false;
-      condicion = "Aprobado";
-    } else if (
-      primerParcial < 4 &&
-      primerParcial <= 10 &&
-      segundoParcial >= 4 &&
-      segundoParcial <= 10 &&
-      asistencia >= 30 * 0.8 &&
-      asistencia <= 30
-    ) {
-      alert("Para poder aprobar el curso deberas recuperar el Primer Parcial.");
-      loop = false;
-      condicion = "Condicional";
-    } else if (
-      primerParcial < 4 &&
-      primerParcial <= 10 &&
-      segundoParcial >= 4 &&
-      segundoParcial <= 10 &&
-      asistencia < 30 * 0.8 &&
-      asistencia <= 30
-    ) {
-      alert(
-        "No has asistido al 80% de las clases ni tampoco aprobado el Primer Parcial, deberás volver a tomar el curso."
-      );
-      loop = false;
-      condicion = "Reprobado";
-    } else if (
-      primerParcial >= 4 &&
-      primerParcial <= 10 &&
-      segundoParcial < 4 &&
-      segundoParcial <= 10 &&
-      asistencia >= 30 * 0.8 &&
-      asistencia <= 30
-    ) {
-      alert(
-        "Para poder aprobar el curso deberas recuperar el Segundo Parcial."
-      );
-      loop = false;
-      condicion = "Condicional";
-    } else if (
-      primerParcial >= 4 &&
-      primerParcial <= 10 &&
-      segundoParcial < 4 &&
-      segundoParcial <= 10 &&
-      asistencia < 30 * 0.8 &&
-      asistencia <= 30
-    ) {
-      alert(
-        "No has asistido al 80% de las clases ni tampoco aprobado el Segundo Parcial, deberás volver a tomar el curso."
-      );
-      loop = false;
-      condicion = "Reprobado";
-    } else if (
-      primerParcial >= 4 &&
-      primerParcial <= 10 &&
-      segundoParcial >= 4 &&
-      segundoParcial <= 10 &&
-      asistencia < 30 * 0.8 &&
-      asistencia <= 30
-    ) {
-      alert(
-        "Has cumplimentado con la nota de los parciales pero no has asistido al 80% de las clases. Por lo tanto, deberás volver a recursar."
-      );
-      loop = false;
-      condicion = "Reprobado";
-    } else {
-      alert("Ingresa un valor válido por favor.");
-      loop = false;
-      condicion =
-        "Hasta que no ingreses valores válidos no vamos a poder indicarte tu condición.";
-    }
+class Monster {
+  constructor(name, hp, atk, def, spd) {
+    this.name = name;
+    this.hp = hp;
+    this.atk = atk;
+    this.def = def;
+    this.spd = spd;
   }
-  return condicion;
+  atack() {
+    console.log(
+      this.name + " ha atacado y ha realizado " + this.atk + " puntos de daño."
+    );
+  }
+  flee() {
+    console.log(this.name + " ha huido del combate.");
+  }
 }
 
-let condicionFinal = condicionAlumno(
-  parseInt(prompt("Ingresa la nota que te sacaste en el Primer Parcial: ")),
-  parseInt(prompt("Ingresa la nota que te sacaste en el Segundo Parcial: ")),
-  parseInt(prompt("Ingresa la cantidad de clases a la que has asistido: "))
-);
+let ogre = new Monster("Ogro", 300, 45, 70, 30);
+let imp = new Monster("Duende", 80, 15, 10, 10);
+let dragon = new Monster("Dragon", 450, 90, 85, 80);
+let centaur = new Monster("Centauro", 150, 30, 42, 50);
+let manticore = new Monster("Mantícora", 230, 60, 40, 60);
 
-alert("Tu condición como alumnx es: " + condicionFinal);
+const monsterArray = [ogre, imp, dragon, centaur];
+monsterArray.push(manticore);
+
+const monsterDetail = [ogre, imp, dragon, centaur, manticore];
+
+let monsterName = monsterArray.map((item) => item.name);
+
+function monsterList() {
+  let index = 0;
+  for (x in monsterName) {
+    console.log(x + " " + monsterName[index]);
+    index++;
+  }
+}
+
+function choice(choice) {
+  if (choice == 0) {
+    alert(
+      "Has elegido combatir contra " +
+        ogre.name +
+        " HP: " +
+        ogre.hp +
+        " ATK: " +
+        ogre.atk +
+        " DEF: " +
+        ogre.def +
+        " SPD: " +
+        ogre.spd
+    );
+  } else if (choice == 1) {
+    alert(
+      "Has elegido combatir contra " +
+        imp.name +
+        " HP: " +
+        imp.hp +
+        " ATK: " +
+        imp.atk +
+        " DEF: " +
+        imp.def +
+        " SPD: " +
+        imp.spd
+    );
+  } else if (choice == 2) {
+    alert(
+      "Has elegido combatir contra " +
+        dragon.name +
+        " HP: " +
+        dragon.hp +
+        " ATK: " +
+        dragon.atk +
+        " DEF: " +
+        dragon.def +
+        " SPD: " +
+        dragon.spd
+    );
+  } else if (choice == 3) {
+    alert(
+      "Has elegido combatir contra " +
+        centaur.name +
+        " HP: " +
+        centaur.hp +
+        " ATK: " +
+        centaur.atk +
+        " DEF: " +
+        centaur.def +
+        " SPD: " +
+        centaur.spd
+    );
+  } else if (choice == 4) {
+    alert(
+      "Has elegido combatir contra " +
+        manticore.name +
+        " HP: " +
+        manticore.hp +
+        " ATK: " +
+        manticore.atk +
+        " DEF: " +
+        manticore.def +
+        " SPD: " +
+        manticore.spd
+    );
+  } else {
+    alert("Selecciona una opción valida");
+  }
+}
+
+monsterList();
+choice(
+  parseInt(
+    prompt("Elige el número de monstruo contra el que quieres combatir: ")
+  )
+);
