05de65fd4a55cdcec03bb3bbba097304af3d34fe
Lucio Ramos <lucio.a.ramos@gmail.com>
2022-07-12 14:28:20 -0300

    Se agrega constructor de objeto, creacion de objetos, arrays, funciones que llaman, todo ebajo la misma tempática

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Buenas tardes.
-En el archivo main.js se encuentra el script de la entrega de Simulador Interactivo.
-En el mismo se solicita al alumnx que indique las notas de sus dos parciales y la cantidad de clases asistidas.
-De esta manera se le indica cual es su condición de cursada.
-También se agrega un pequeño header como para tener un mínimo contenido en la página.
+En esta primera entrega del trabajo me terminé decidiendo por hacer una web de lo que sería un curso en el cual va a tener links que te lleven a las diferentes páginas desde el header. Va a tener una sección de estudiantes, de notas, y capaz agregue alguna que se me ocurra mas adelante.
+La idea seria que la página principal haya una bienvenida con información correspondiente al curso. Que luego en la seccion de alumnos puedan acceder a la lista y detalles de los mismos.
+Que en la sección nota deban ingresar su asistencia y notas de parciales para verificar su condición.
+En el archivo main.js se encuentra el script en el cual se solicita al alumnx que indique las notas de sus dos parciales y la cantidad de clases asistidas.
+De esta manera se le indica cual es su condición de cursada. A su vez se realiza una lista de los alumnos que forman parte del curso, se puede visualizar en la consola.
--- main.js
+++ main.js
@@ -1,11 +1,11 @@
+/* condicionales y simulador de condicion de alumnx */
+
 let primerParcial;
 let segundoParcial;
 let asistencia;
 let condicion;
 let loop = true;
-alert(
-  "Para aprobar este curso es necesario tener nota mayor a 7 en los dos parciales, ademas tener el 80% asistencia de las 30 clases dictadas."
-);
+
 function condicionAlumno(primerParcial, segundoParcial, asistencia) {
   while (loop) {
     if (
@@ -92,10 +92,60 @@ function condicionAlumno(primerParcial, segundoParcial, asistencia) {
   return condicion;
 }
 
-let condicionFinal = condicionAlumno(
-  parseInt(prompt("Ingresa la nota que te sacaste en el Primer Parcial: ")),
-  parseInt(prompt("Ingresa la nota que te sacaste en el Segundo Parcial: ")),
-  parseInt(prompt("Ingresa la cantidad de clases a la que has asistido: "))
-);
+setTimeout(() => {
+  alert(
+    "Para aprobar este curso es necesario tener nota mayor a 7 en los dos parciales, ademas tener el 80% asistencia de las 30 clases dictadas."
+  );
+  let condicionFinal = condicionAlumno(
+    parseInt(prompt("Ingresa la nota que te sacaste en el Primer Parcial: ")),
+    parseInt(prompt("Ingresa la nota que te sacaste en el Segundo Parcial: ")),
+    parseInt(prompt("Ingresa la cantidad de clases a la que has asistido: "))
+  );
 
-alert("Tu condición como alumnx es: " + condicionFinal);
+  alert("Tu condición como alumnx es: " + condicionFinal);
+}, 5000);
+
+/* Objetos de estudiantes con array */
+
+class Estudiantes {
+  constructor(nombre, apellido, dni, comision) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.comision = comision;
+  }
+  aprobar() {
+    console.log("Genial! Aprobé el curso!");
+  }
+  desaprobar() {
+    console.log("No pude aprobar, que tristeza.");
+  }
+}
+
+let estudiante1 = new Estudiantes("Juan", "Perez", 21456923, 1);
+let estudiante2 = new Estudiantes("Pepe", "Grillo", 33548962, 2);
+let estudiante3 = new Estudiantes("Juan Carlos", "Orzuelo", 27563123, 3);
+let estudiante4 = new Estudiantes("Milena", "Grido", 11548963, 1);
+let estudiante5 = new Estudiantes("Sofía", "Corriente", 41852357, 2);
+let estudiante6 = new Estudiantes("Carla", "Poten", 68312548, 3);
+
+const estudiantesArray = [
+  estudiante1,
+  estudiante2,
+  estudiante3,
+  estudiante4,
+  estudiante5,
+];
+estudiantesArray.push(estudiante6);
+
+let apellidoEstudiantes = estudiantesArray.map((item) => item.apellido);
+
+function listaEstudiantes() {
+  let index = 0;
+  for (x in apellidoEstudiantes) {
+    console.log(x + " " + apellidoEstudiantes[index]);
+    index++;
+  }
+}
+
+listaEstudiantes();
