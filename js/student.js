e3679ef6c3c4cbcc37512ae87e8dac42e5cef60f
Lucio Ramos <lucio.a.ramos@gmail.com>
2022-07-27 16:08:46 -0300

    se agrega JSON y localStorage en student.js

--- js/student.js
+++ js/student.js
@@ -22,15 +22,26 @@ let estudiante6 = new Estudiantes('Carla', 'Poten', 68312548, 3);
 const containerStudent = document.getElementById('container_student');
 const boton = document.getElementById('boton');
 let contenedor = document.createElement('div');
-const estudiantesArray = [estudiante1, estudiante2, estudiante3, estudiante4, estudiante5];
-estudiantesArray.push(estudiante6);
-
+const estudiantesArray = [estudiante1, estudiante2, estudiante3, estudiante4, estudiante5, estudiante6];
+let aux;
+let aux2;
+let aux3;
 for (const estudiante of estudiantesArray) {
   let contenedor = document.createElement('div');
   contenedor.innerHTML = `<p> <strong>Nombre: </strong> ${estudiante.nombre}</p>
-                                <p> <strong>Apellido: </strong> ${estudiante.apellido}</p>
-                                <p> <strong>DNI: </strong> ${estudiante.dni}</p>
-                                <p> <strong>Comisión: </strong> ${estudiante.comision}</p>`;
+                                  <p> <strong>Apellido: </strong> ${estudiante.apellido}</p>
+                                  <p> <strong>DNI: </strong> ${estudiante.dni}</p>
+                                  <p> <strong>Comisión: </strong> ${estudiante.comision}</p>`;
+  containerStudent.appendChild(contenedor);
+}
+
+function agregarALista() {
+  contenedor.innerHTML = `
+        <p> <strong>Nombre: </strong>  ${aux2.nombre}</p>
+        <p><strong>Apellido: </strong>: ${aux2.apellido}</p>
+        <p><strong>DNI: </strong> ${aux2.dni}</p>
+        <p><strong>Comisión: </strong> ${aux2.comision}</p>
+    `;
   containerStudent.appendChild(contenedor);
 }
 
@@ -39,12 +50,18 @@ boton.addEventListener('click', () => {
   let apellido = document.getElementById('apellido').value;
   let dni = document.getElementById('dni').value;
   let comision = document.getElementById('comision').value;
-
-  contenedor.innerHTML = `
-        <p> <strong>Nombre: </strong>  ${nombre}</p>
-        <p><strong>Apellido: </strong>: ${apellido}</p>
-        <p><strong>DNI: </strong> ${dni}</p>
-        <p><strong>Comisión: </strong> ${comision}</p>
-    `;
-  containerStudent.appendChild(contenedor);
+  aux2 = new Estudiantes(nombre, apellido, dni, comision);
+  aux3 = JSON.stringify(aux2);
+  localStorage.setItem('Estudiante', aux3);
+  agregarALista();
 });
+
+let aux4 = localStorage.getItem('Estudiante');
+aux = JSON.parse(aux4);
+
+contenedor = document.createElement('div');
+contenedor.innerHTML = `<p> <strong>Nombre: </strong> ${aux.nombre}</p>
+                                  <p> <strong>Apellido: </strong> ${aux.apellido}</p>
+                                  <p> <strong>DNI: </strong> ${aux.dni}</p>
+                                  <p> <strong>Comisión: </strong> ${aux.comision}</p>`;
+containerStudent.appendChild(contenedor);
